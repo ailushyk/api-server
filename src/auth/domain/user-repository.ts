2ccb9fd8a3a3c0eaf145fa2cf9b5579dce29425b
Alex Ilushyk <ailushyk@gmail.com>
2024-08-16 00:32:11 +0200

    chore: remove deprecated auth module

--- src/auth/domain/user-repository.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '@/auth/domain/user'
-
-export interface UserRepository {
-  createIfNotExists({ id }: { id: string }): Promise<User>
-  // updateLastLoginAt(id: string): Promise<void>
-}
